c94ef013f3f91c9c64391bf9be4def3939b49b36
Sebastian Ortiz <dsebaortiz@gmail.com>
2021-10-22 23:05:58 -0300

    Init repo

---
+++ webpack.mix.js
@@ -0,0 +1,7 @@
+const mix = require("laravel-mix");
+
+const RESOURCES_DIR = './assets/';
+const DIST_DIR = './dist/';
+
+mix.js(`${RESOURCES_DIR}/scripts/app.js`, DIST_DIR);
+mix.sass(`${RESOURCES_DIR}/styles/styles.scss`, DIST_DIR);
