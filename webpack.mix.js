1cae3b170b623c066eb238a882b11ea0049a2629
Sebastian Ortiz <dsebaortiz@gmail.com>
2021-10-23 02:03:29 -0300

    Basic structure for FSE

--- webpack.mix.js
+++ webpack.mix.js
@@ -4,4 +4,5 @@ const RESOURCES_DIR = './assets/';
 const DIST_DIR = './dist/';
 
 mix.js(`${RESOURCES_DIR}/scripts/app.js`, DIST_DIR);
-mix.sass(`${RESOURCES_DIR}/styles/styles.scss`, DIST_DIR);
+mix.sass(`${RESOURCES_DIR}/styles/style.scss`, DIST_DIR);
+mix.sass(`${RESOURCES_DIR}/styles/editor.scss`, DIST_DIR);
